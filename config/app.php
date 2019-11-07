266b328bae5abdc1bfc8979be194bf22f6e0f6cf
eliot117 <38276072+eliot117@users.noreply.github.com>
2019-11-07 01:09:45 -0600

    hbk

--- config/app.php
+++ config/app.php
@@ -162,7 +162,6 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
 
-        Collective\Html\HtmlServiceProvider::class,
         /*
          * Package Service Providers...
          */
@@ -227,8 +226,6 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
 
-        'Form' => Collective\Html\FormFacade::class,
-        'Html' => Collective\Html\HtmlFacade::class,
     ],
 
 ];
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,5 @@
 <?php
 
-use Spatie\Permission\Models\Role;
-use Spatie\Permission\Models\Permission;
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -17,23 +14,3 @@ use Spatie\Permission\Models\Permission;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
-
-Route::resource('users', 'UserController');
-
-Route::resource('roles', 'RoleController');
-
-Route::resource('permissions', 'PermissionController');
-
-Route::resource('posts', 'PostController');
-
-//Route::get('/create_role_permission', function(){
-  //  $role = Role::create(['name' => 'Administer']);
-    //$permission = Permission::create(['name' => 'Administer roles & permissions']);
-    //auth()->user()->assingRole('Administer');
-    //auth()->user()->givePermissionTo('Administer roles & permissions');
-//});
-
